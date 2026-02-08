190db6d1cb4e5e954b7e6280d07a11572efc05dc
Furkann <therealfurkanking@gmail.com>
2026-02-08 14:30:34 +0300

    Initial Start
    0.1 Prototype

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Selam!");
+function cevapGoster(button) {
+    const cevap = button.parentElement.nextElementSibling;
+
+    if (cevap.style.display === "none" || cevap.style.display === "") {
+        cevap.style.display = "block";
+        button.textContent = "Cevabı Gizle";
+    } else {
+        cevap.style.display = "none";
+        button.textContent = "Cevabı Göster";
+    }
+}
